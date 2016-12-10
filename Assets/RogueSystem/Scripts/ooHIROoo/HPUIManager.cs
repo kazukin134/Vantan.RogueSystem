e6cf85f4692dd8df6c39059513f383bf5e82aad2
ooHIROoo <hiro1215.turquoise@gmail.com>
2016-12-10 16:34:16 +0900

    Gameの仮UI作成

---
+++ Assets/RogueSystem/Scripts/ooHIROoo/HPUIManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class HPUIManager : MonoBehaviour {
+
+    [SerializeField]
+    Image _image = null;
+	
+	void Update ()
+    {
+        // プレイヤーの情報から取得できるようにする
+        // current_hp / max_hp
+        _image.fillAmount = 1.0f;
+	}
+}
---
+++ Assets/RogueSystem/Scripts/ooHIROoo/MenuActiver.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuActiver : MonoBehaviour {
+
+    [SerializeField]
+    GameObject _menuUI = null;
+
+	
+	void Update () {
+        if (!Input.GetMouseButtonDown(1)) return;
+        _menuUI.SetActive(!_menuUI.activeSelf);
+	}
+}
